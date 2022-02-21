f1bfa0c1eade90fb6dd8e2d4d50dc7bc92b0969b
Javedansaari <javedansari10100@gmail.com>
2022-02-21 23:31:18 +0545

    comment added

--- Interview Program/Strings/CountString.java
+++ Interview Program/Strings/CountString.java
@@ -5,12 +5,12 @@ public class CountString {
         int count = 0;
         int startWith = 0;
 
-        for(;;){
+        for(;;){  //empty for loop is used for infinite looping 
             int index = str.indexOf('e',startWith);
 
             if(index >= 0){
                 count += 1;
-                startWith = index + 1;
+                startWith = index + 1; //once e is found on certain index, the next startwith should starts with next of the index found. for eg. if 'e' is found on index 1, the loop should continue with index 2 onward.
             }else{
                 break;
             }
