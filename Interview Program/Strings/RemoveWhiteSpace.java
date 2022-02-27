99159a261190add95f1e3351c334141fa9fcaa92
Javedansaari <javedansari10100@gmail.com>
2022-02-27 12:23:10 +0545

    remove white space in a string

---
+++ Interview Program/Strings/RemoveWhiteSpace.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+
+public class RemoveWhiteSpace {
+    public static void main(String args[]){
+        String str = "    hey this is  javed   ansari        ";
+        int i;
+        
+        char[] ch = new char[str.length()];
+        for(i=0 ; i<str.length() ; i++){
+            ch[i] = str.charAt(i);
+            if(i>0 && ch[str.length()-1] != ' ' && ch[i] == ' '   ||  ch[0] == ' ' && i==0){
+                ch[i]=ch[i+1];
+            }
+        }      
+          
+        String str1 = String.copyValueOf(ch);
+        System.out.println(str1);
+    }
+}
